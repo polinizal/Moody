953f090ec815c25dfb8e16a5328a295d7f1c78f3
polinizal <polina.staneva@yahoo.com>
2024-04-11 19:44:22 +0300

    add repositories

---
+++ Moody.Data/Repositories/Abstractions/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using Moody.Data.Data.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moody.Data.Repositories.Abstractions
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate);
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteByIdAsync(int id);
+        
+    }
+} 
