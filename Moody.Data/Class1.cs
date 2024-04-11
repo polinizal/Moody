1d70ccf71e7aa053042a05835b300dcb8a390b05
polinizal <polina.staneva@yahoo.com>
2024-04-11 16:02:03 +0300

    Add project files.

---
+++ Moody.Data/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Moody.Data
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Moody/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Moody.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
