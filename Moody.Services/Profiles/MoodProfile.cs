0996a33705abe98c65f7a3e29194dd0528795309
polinizal <polina.staneva@yahoo.com>
2024-04-14 08:29:24 +0300

    edit profiles

---
+++ Moody.Services/Profiles/MoodProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Moody.Data.Data;
+using Moody.Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moody.Services.Profiles
+{
+    public class MoodProfile: Profile
+    {
+        public MoodProfile()
+        {
+            CreateMap<Mood, MoodDTO>()
+                .ReverseMap();
+            
+        }   
+    }
+}
---
+++ Moody.Services/Profiles/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Moody.Data.Data;
+using Moody.Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moody.Services.Profiles
+{
+    public class UserProfile:Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserDTO>()
+                .ReverseMap();
+        }
+    }
+}
