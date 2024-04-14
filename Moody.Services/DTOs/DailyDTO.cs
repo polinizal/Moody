ca8e7af07d15f73b0fa9f5c9f094a8d49c95b2ef
polinizal <polina.staneva@yahoo.com>
2024-04-14 08:29:36 +0300

    edit DTOs

---
+++ Moody.Services/DTOs/DailyDTO.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moody.Services.DTOs
+{
+    public class DailyDTO : BaseDTO
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string UserId { get; set; }
+        public string Note { get; set; }
+        public int MoodId { get; set; }
+        
+        public virtual MoodDTO? Mood { get; set; }
+        public virtual UserDTO? User { get; set; }
+    }
+
+}
---
+++ Moody.Services/DTOs/ModDTO.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moody.Services.DTOs
+{
+    public class MoodDTO : BaseDTO
+    {
+        public string MoodType { get; set; }
+    }
+}
