8086652866f534332f7dd45622357181099868b5
polinizal <polina.staneva@yahoo.com>
2024-04-14 08:32:20 +0300

    modify program.cs

--- Moody/Program.cs
+++ Moody/Program.cs
@@ -1,6 +1,11 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Moody.Data;
+using AutoMapper;
+using Moody.Services.Services.Abstractions;
+using Moody.Services.Services;
+using Moody.Data.Repositories.Abstractions;
+using Moody.Data.Repositories;
 
 namespace Moody
 {
@@ -13,12 +18,24 @@ namespace Moody
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+            {
+                options.UseSqlServer(connectionString);
+                options.UseLazyLoadingProxies();
+            });
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            builder.Services.AddScoped(typeof(IUserRepository), typeof(UserRepository));
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IMoodService, MoodService>();
+            builder.Services.AddScoped<IDailyService, DailyService>();
+
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             var app = builder.Build();
 
