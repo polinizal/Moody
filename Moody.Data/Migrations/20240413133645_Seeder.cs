60c5985db9190d57faa588c6bf6b47df950ff613
polinizal <polina.staneva@yahoo.com>
2024-04-14 08:30:27 +0300

    add migrations and edit ApplicationDbContext

---
+++ Moody.Data/Migrations/20240413133645_Seeder.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Moody.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seeder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Moods",
+                columns: new[] { "Id", "MoodType" },
+                values: new object[,]
+                {
+                    { 1, "Happy" },
+                    { 2, "Excited" },
+                    { 3, "Cheerful" },
+                    { 4, "Optimistic" },
+                    { 5, "Relaxed" },
+                    { 6, "Sad" },
+                    { 7, "Angry" },
+                    { 8, "Tired" },
+                    { 9, "Stressed" },
+                    { 10, "Anxious" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Moods",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Moods",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Moods",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Moods",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Moods",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Moods",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Moods",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Moods",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Moods",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Moods",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
